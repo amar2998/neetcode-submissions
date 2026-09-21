1bec6580182f12581e90bfca8bf52b6a96bde07e
amar2998 <160626597+amar2998@users.noreply.github.com>
2026-09-21 23:14:23 +0530

    Add: longest-palindromic-substring - submission-7

---
+++ Data Structures & Algorithms/longest-palindromic-substring/submission-7.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    string longestPalindrome(string s) {
+        int n=s.size();
+        vector<vector<int>> dp(n,vector<int>(n,false));
+        int resInd=0;
+        int resLen=0;
+        for(int i=n-1;i>=0;i--){
+            for(int j=i;j<n;j++){
+                if(s[i]==s[j] && ((j-i <=2 || dp[i+1][j-1]))){
+                    dp[i][j]=true;
+                    if(resLen < (j-i+1)){
+                        resLen=j-i+1;
+                        resInd=i;
+                    }
+                }
+            }
+        }
+        return s.substr(resInd,resLen);
+    }
+};
